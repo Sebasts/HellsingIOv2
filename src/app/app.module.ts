6ea8f04538b3af2d02438da457d15911ebca5fc8
hells <hells@DESKTOP-ES2F4O8>
2018-07-25 18:32:27 -0600

    Initial Commit - blog services setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogSpotlightComponent } from './blog-spotlight/blog-spotlight.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlogComponent,
+    BlogSpotlightComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
