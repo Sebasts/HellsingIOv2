10c42aee2a2f3f02c66ce33c225f9fb93db0d754
sebasts <sebastsing@gmail.com>
2019-02-23 14:03:02 -0700

    Adding updates to resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { $ } from 'jquery'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    onInit() {
+        $(window).scroll(function () {
+            sessionStorage.scrollTop = $(this).scrollTop();
+        });
+
+        $(document).ready(function () {
+            if (sessionStorage.scrollTop !== 'undefined') {
+                $(window).scrollTop(sessionStorage.scrollTop);
+            }
+        });
+    }
 }
