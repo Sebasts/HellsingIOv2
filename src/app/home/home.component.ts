c7f00c8a14577a79afc58c39d8f43d79857a6097
Hellsing <hellsis@essendant.com>
2019-03-27 20:10:57 -0600

    Adding user printouts for google login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,12 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  public onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
 }
